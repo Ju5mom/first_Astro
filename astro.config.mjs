242c7279fcad7e7bc63eaee6eab638817ceee314
Masayo Ishibashi <m.ishibashi@enjoyworks.jp>
2025-10-23 16:05:56 +0900

    preactの導入と島コンポーネント追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://warm-rabanadas-293b08.netlify.app/"
+  site: "https://warm-rabanadas-293b08.netlify.app/",
+  integrations: [preact()]
 });
